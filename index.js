cdb49b4b2d116c5647397a2409cc87ce96baf70f
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-14 20:43:54 -0400

    allow user to disable trailing slash removal

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /*!
  * unixify <https://github.com/jonschlinkert/unixify>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2014, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
 
-var normalize = require('normalize-path');
+var normalizePath = require('normalize-path');
 
-module.exports = function unixify(fp) {
-  return normalize(fp).replace(/^\w+:/, '');
+module.exports = function unixify(filepath, stripTrailing) {
+  filepath = normalizePath(filepath, stripTrailing);
+  return filepath.replace(/^([a-zA-Z]+:|\.\/)/, '');
 };
--- test.js
+++ test.js
@@ -4,32 +4,43 @@ require('mocha');
 var assert = require('assert');
 var unixify = require('./');
 
-it('should convert backslashes to forward slashes:', function () {
-  assert.equal(unixify('one\\two\\three'), 'one/two/three');
+describe('unixify', function() {
+  it('should convert backslashes to forward slashes', function() {
+    assert.equal(unixify('one\\two\\three'), 'one/two/three');
+  });
+
+  it('should strip trailing slashes', function() {
+    assert.equal(unixify('one/two/three/'), 'one/two/three');
+    assert.equal(unixify('one\\two\\three\\'), 'one/two/three');
+  });
+
+  it('should not strip trailing slashes when disabled', function() {
+    assert.equal(unixify('one\\two\\three\\', false), 'one/two/three/');
+    assert.equal(unixify('one/two/three/', false), 'one/two/three/');
+  });
+
+  it('should strip leading `./`', function() {
+    assert.equal(unixify('.\\one\\two\\three\\'), 'one/two/three');
+    assert.equal(unixify('./one/two/three/'), 'one/two/three');
+  });
+
+  it('should condense multiple slashes to one', function() {
+    assert.equal(unixify('one\\two\\//three'), 'one/two/three');
+    assert.equal(unixify('one//two//////three'), 'one/two/three');
+  });
+
+  it('should strip windows drive letters', function() {
+    assert.equal(unixify('C:////one//two//three'), '/one/two/three');
+    assert.equal(unixify('C:////one//two//three'), '/one/two/three');
+    assert.equal(unixify('C://one//two//three'), '/one/two/three');
+    assert.equal(unixify('C:/one/two/three'), '/one/two/three');
+    assert.equal(unixify('C:\\//one\\//two\\//three'), '/one/two/three');
+    assert.equal(unixify('C:\\//one\\two\\three'), '/one/two/three');
+    assert.equal(unixify('C:\\\\one\\two\\three'), '/one/two/three');
+    assert.equal(unixify('C:\\one\\two\\three'), '/one/two/three');
+  });
+
+  it('should condense multiple slashes following drive letter', function() {
+    assert.equal(unixify('C:\\//one\\two\\//three'), '/one/two/three');
+  });
 });
-
-it('should strip trailing slashes:', function () {
-  assert.equal(unixify('one\\two\\three\\'), 'one/two/three');
-});
-
-it('should strip leading `./`:', function () {
-  assert.equal(unixify('.\\one\\two\\three\\'), 'one/two/three');
-  assert.equal(unixify('./one/two/three/'), 'one/two/three');
-});
-
-it('should strip windows drive letters:', function () {
-  assert.equal(unixify('F:\\one\\two\\three'), '/one/two/three');
-  assert.equal(unixify('F:\\//one\\two\\three'), '/one/two/three');
-  assert.equal(unixify('F:\\//one\\two\\three'), '/one/two/three');
-});
-
-it('should condense multiple slashes to one:', function () {
-  assert.equal(unixify('one\\two\\//three'), 'one/two/three');
-  assert.equal(unixify('one//two//////three'), 'one/two/three');
-});
-
-it('should condense multiple slashes following drive letter:', function () {
-  assert.equal(unixify('C:\\//one\\two\\//three'), '/one/two/three');
-});
-
-
